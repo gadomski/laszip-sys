c6ba7bd65ad9dc0277adf9938fca3764dad2ad33
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-20 12:44:10 -0600

    Add get_point_count

--- src/reader.rs
+++ src/reader.rs
@@ -1,11 +1,13 @@
 use {Dll, Result};
 use laszip_sys;
+use std::os::raw::c_void;
 use std::path::Path;
 use std::ptr;
 
 pub struct Reader {
-    _dll: Dll,
     is_compressed: bool,
+    pointer: *mut c_void,
+    _dll: Dll,
 }
 
 impl Reader {
@@ -14,9 +16,9 @@ impl Reader {
 
         let path = CString::new(path.as_ref().to_string_lossy().as_ref())?;
         let mut is_compressed = 0;
+        let mut pointer = ptr::null_mut();
         let dll = Dll::load()?;
         unsafe {
-            let mut pointer = ptr::null_mut();
             laszip_try_without_pointer!(laszip_sys::laszip_create(&mut pointer));
             laszip_try!(
                 laszip_sys::laszip_open_reader(pointer, path.as_ptr(), &mut is_compressed),
@@ -24,13 +26,25 @@ impl Reader {
             );
         }
         Ok(Reader {
-            _dll: dll,
             is_compressed: if is_compressed == 0 { false } else { true },
+            pointer: pointer,
+            _dll: dll,
         })
     }
 
     pub fn is_compressed(&self) -> bool {
         self.is_compressed
+    }
+
+    pub fn point_count(&self) -> Result<i64> {
+        let mut count = 0;
+        unsafe {
+            laszip_try!(
+                laszip_sys::laszip_get_point_count(self.pointer, &mut count),
+                self.pointer
+            );
+        }
+        Ok(count)
     }
 }
 
@@ -42,5 +56,11 @@ mod tests {
     fn from_path() {
         let reader = Reader::from_path("data/autzen.laz").unwrap();
         assert!(reader.is_compressed());
+    }
+
+    #[test]
+    fn point_count() {
+        let reader = Reader::from_path("data/autzen.laz").unwrap();
+        assert_eq!(0, reader.point_count().unwrap());
     }
 }
